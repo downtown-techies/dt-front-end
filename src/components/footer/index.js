c2f8e7d1a476096bd3773d904eea807dcde0b17b
Blake Burnette <jbburnette2@gmail.com>
2019-03-30 15:21:33 -0400

    Slight styling to footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import styled from '../../theme';
 
 const StyledFooter = styled.div`
+  background-color: red;
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  padding: 1rem 10vw;
+  z-index: 1;
+  position: relative;
 `;
 
 class Footer extends Component {
