559a0ed5bcc3870d73b58e30482ba22adf8a466e
Blake Burnette <jbburnette2@gmail.com>
2020-01-10 00:00:16 -0500

    sample protected route

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -4,12 +4,14 @@ import NotFound from '../NotFound';
 import Login from '../Login';
 import AddUser from '../forms/AddUserForm';
 import AddAccount from '../forms/AddAccountForm';
+import DeleteUserForm from '../forms/DeleteUserForm';
 import jwt from 'jwt-decode';
 
 import {
   BrowserRouter as Router,
   Route,
   Switch,
+  Redirect
 } from 'react-router-dom';
 
 const jwtToken = localStorage.token;
@@ -28,6 +30,14 @@ if (jwtToken.length > 1){
   accountType = data.accountType
 }
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+   jwtToken.length > 1 
+      ? <Component {...props} />
+      : <Redirect to='/login' />
+  )} />
+)
+
 // <Route exact={true} path="/nested_admin/add_meetup" component={AddMeetup} /> 
 // <Route exact={true} path="/delete" component={DeleteUser} /> 
 
@@ -38,6 +48,7 @@ function Main() {
         <Switch>
           <Route exact={true} path="/signup" component={AddUser} /> 
           <Route exact={true} path="/addaccount" component={AddAccount} /> 
+          {/* <PrivateRoute exact={true} path="/delete-meetup" component={DeleteUserForm} /> */}
           <Route exact={true} path="/login" component={Login} />
           <Route exact={true} path="/" component={Landing} />
           <Route component={NotFound} />
