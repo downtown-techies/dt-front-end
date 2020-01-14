171ad6e7b09b58211826c8aa6a82c91f598a3d44
Blake Burnette <jbburnette2@gmail.com>
2020-01-14 11:10:49 -0500

    Build package

---
+++ build/precache-manifest.85961a43db665fed96a7af64bc4fc2c5.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "53528fc686d537899009",
+    "url": "/static/css/main.65125c68.chunk.css"
+  },
+  {
+    "revision": "53528fc686d537899009",
+    "url": "/static/js/main.83a5e2f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "555c7cee753293cd5a39",
+    "url": "/static/js/2.fdf4a687.chunk.js"
+  },
+  {
+    "revision": "599dfda78880726695d222e7cb866fca",
+    "url": "/static/media/default.599dfda7.png"
+  },
+  {
+    "revision": "86ba2a65a2e5b81f291656038907bbc9",
+    "url": "/static/media/durham.86ba2a65.jpg"
+  },
+  {
+    "revision": "94a026866e17b9e11081a5725b6dd8da",
+    "url": "/static/media/raleigh.94a02686.jpg"
+  },
+  {
+    "revision": "c63ed58453d5e5eecb319adc36b7fb1a",
+    "url": "/static/media/cityBackground.c63ed584.jpg"
+  },
+  {
+    "revision": "dff30f2f57a2e23132c72ee100f86eb3",
+    "url": "/static/media/mobileCityBackground.dff30f2f.jpg"
+  },
+  {
+    "revision": "b7a3fbb5963e82fccd48d7c856e5dab1",
+    "url": "/static/media/404.b7a3fbb5.png"
+  },
+  {
+    "revision": "1f3ba25f06d315431af2751317690931",
+    "url": "/static/media/AkkuratStd-Light.1f3ba25f.woff"
+  },
+  {
+    "revision": "605b3e2cd719bba96cfd81a2a9e2dd1e",
+    "url": "/static/media/AkkuratStd-Light.605b3e2c.woff2"
+  },
+  {
+    "revision": "7a36f1616ee0c3ce4f0ccd967b784826",
+    "url": "/static/media/AkkuratStd-LightItalic.7a36f161.woff2"
+  },
+  {
+    "revision": "f8458267bae5e02084ba3fc531bd127a",
+    "url": "/static/media/AkkuratStd-LightItalic.f8458267.woff"
+  },
+  {
+    "revision": "d8a83fdef993d62d0b50f6609edd6e76",
+    "url": "/static/media/AkkuratStd.d8a83fde.woff2"
+  },
+  {
+    "revision": "902182f0692a280dca37b1e31305808a",
+    "url": "/static/media/AkkuratStd.902182f0.woff"
+  },
+  {
+    "revision": "4ea45814cfa98d7b64591c6791ad7b37",
+    "url": "/static/media/AkkuratStd-Italic.4ea45814.woff2"
+  },
+  {
+    "revision": "844a2e532a80431e3d899a992daf67cd",
+    "url": "/static/media/AkkuratStd-Italic.844a2e53.woff"
+  },
+  {
+    "revision": "6d618f54ebf3295e4ee6b53ad9d64a7a",
+    "url": "/static/media/AkkuratStd-Bold.6d618f54.woff2"
+  },
+  {
+    "revision": "ccbc1a4f261617d491bc6c459a233d3c",
+    "url": "/static/media/AkkuratStd-Bold.ccbc1a4f.woff"
+  },
+  {
+    "revision": "76a30e555b1651987da56f70662e2565",
+    "url": "/static/media/AkkuratStd-BoldItalic.76a30e55.woff2"
+  },
+  {
+    "revision": "ba047ea8c28e880bada4b03f8f0cea56",
+    "url": "/static/media/AkkuratStd-BoldItalic.ba047ea8.woff"
+  },
+  {
+    "revision": "16e8bab76a42c4bc883d3b6ba2d0cd75",
+    "url": "/static/media/PublicoText-Bold.16e8bab7.woff2"
+  },
+  {
+    "revision": "de80068e64139f2d4d0fb6706c74f920",
+    "url": "/static/media/PublicoText-Bold.de80068e.woff"
+  },
+  {
+    "revision": "c6d88253e4fca7d0039fa0622ae014bc",
+    "url": "/static/media/PublicoText-BoldItalic.c6d88253.woff2"
+  },
+  {
+    "revision": "f048a7cac09b7ce2b60734d0f8655ce0",
+    "url": "/static/media/PublicoText-BoldItalic.f048a7ca.woff"
+  },
+  {
+    "revision": "403211b213f68b2bd8248d482b3e4b45",
+    "url": "/static/media/WRALTechWireSiteLogo.403211b2.png"
+  },
+  {
+    "revision": "bd2a342af1dc18613cb5457a45c7009b",
+    "url": "/index.html"
+  }
+];
