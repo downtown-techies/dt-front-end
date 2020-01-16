614d07406bb2c3bec62ce0962021db9886862713
Blake Burnette <jbburnette2@gmail.com>
2020-01-16 16:07:56 -0500

    Updates name and fixes small glitch

--- src/components/forms/AddUserForm/styles.js
+++ src/components/forms/AddUserForm/styles.js
@@ -6,7 +6,7 @@ import Button from '../../shared/Button';
 
 const StyledAddUser = styled.div`
   width: 80%;
-  padding: 5vh 0;
+  padding: 5vh 0 25vh;
   min-height: 70vh;
   margin: 0 auto;
   @media (min-width: 1024px) {
